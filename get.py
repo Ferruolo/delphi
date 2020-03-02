c1fdefef98008df04a6a37631a9a4adfbf6dd158
Ferruolo <Andrew.Ferruolo@users.noreply.github.com>
2020-03-02 16:16:07 -0500

    Created data input, idea for architecture for predicting wit headlines stockRegNet

--- get.py
+++ get.py
@@ -1,6 +1,7 @@
 
-
 import requests
+import io
+import pandas
 from bs4 import BeautifulSoup as BS
 import nltk
 
@@ -12,4 +13,19 @@ def News(ticker):
     News = [nltk.word_tokenize(h) for h in News]
     return News
                   
-                  
+
+api_key = 'MZE3U0MSR1DCE53Z'
+
+
+def weekly(ticker, outputsize = 'compact'):
+    return pandas.read_csv(io.StringIO(requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&&symbol={ticker}&apikey={api_key}&outputsize={outputsize}&datatype=csv').content.decode('utf-8')))
+    
+  
+
+def intraday_data(ticker, time='1min', outputsize = 'compact'):
+    return pandas.read_csv(io.StringIO(requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval={time}&apikey={api_key}&outputsize={outputsize}&datatype=csv').content.decode('utf-8')))
+
+
+def tickers():
+    return pandas.read_csv("NYSE_TICKERS.csv").iloc[:,0]
+    00
